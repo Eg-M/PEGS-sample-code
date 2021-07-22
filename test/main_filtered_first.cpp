f83563837b8d97d886db417628018afc0032d9d3
Eg-M <musaev.egit@gmail.com>
2021-07-22 14:45:16 +0200

    Major_changes

--- test/main_filtered_first.cpp
+++ test/main_filtered_first.cpp
@@ -15,8 +15,8 @@ Timer t;
 //AnalogIn   Sens5(A4);
 //AnalogIn   Sens6(A5);
 
-AnalogIn Sens[6] = {A0, A1, A2, A3, A4, A5};
-AnalogIn adc_vref(ADC_VREF);
+AnalogIn Sens[7] = {A0, A1, A2, A3, A4, A5, ADC_VREF};
+//AnalogIn adc_vref(ADC_VREF);
 
    
 //unsigned short Sensors[6] = {Sens1.read_u16(),Sens2.read_u16(),Sens3.read_u16(),Sens4.read_u16(),Sens5.read_u16(),Sens6.read_u16()};
@@ -26,17 +26,17 @@ AnalogIn adc_vref(ADC_VREF);
 //float Res1 = 1.775;
 float Vout[6] = {0.0f};
 float Res_PBGS[6] = {0.0f};
-float Res1[6] = {5.2419f, 5.234f, 5.234f, 5.258f, 5.1879f, 5.067f};
+float Res1[6] = {5.2f, 5.234f, 5.3f, 5.258f, 5.243f, 5.06f};
 //float filter_val1,filter_val2,filter_val3,filter_val4,filter_val5,filter_val6 = 0;
-float filter_val[6] = {0.0f};
+float filter_val[7] = {0.0f};
 uint16_t Max_res = 0xFFFF;
 Serial pc(USBTX, USBRX); // tx, rx
 
 const uint8_t iter = 20;
-unsigned short buf[6][iter] = {0};
-unsigned short max_buf[6], min_buf[6] = {0};
-unsigned int sum_buf[6] = {0};
-float pseumed[6] = {0.0f};
+unsigned short buf[7][iter] = {0};
+unsigned short max_buf[7], min_buf[7] = {0};
+unsigned int sum_buf[7] = {0};
+float pseumed[7] = {0.0f};
 float vref=0.0f;
 uint16_t vref12 = 0;
 
@@ -55,14 +55,14 @@ int main()
         //{
         if (!(t.read_ms() % 1000))
         {
-             vref12 = adc_vref.read_u16() >> 4;
-             vref = (3.0f * (*VREFINT_CAL_ADDR) / vref12 );
+            //uint16_t vref12 = adc_vref.read_u16() >> 4;
+            //float vref = (3.0f * (*VREFINT_CAL_ADDR) / vref12 );
 
 
             for (u_int8_t ch = 0; ch < 6; ch++)
             {
-                Vout[ch] = (filter_val[ch] / Max_res) * vref;
-                Res_PBGS[ch] = Res1[ch] * Vout[ch] / (vref - Vout[ch]);
+                Vout[ch] = ((filter_val[ch]*filter_val[6]*8.5715f)/ Max_res)/Max_res ;
+                Res_PBGS[ch] = Res1[ch] * Vout[ch] / ((filter_val[6]*8.5715f/Max_res) - Vout[ch]);
             }
 
             printf("%f,", Res_PBGS[0]);
@@ -73,11 +73,11 @@ int main()
             printf("%f, ", Res_PBGS[5]);
             
 
-            printf("%f\n", vref);
+            printf("%f\n", ((filter_val[6]*8.5715f/Max_res)));
             for (u_int8_t i = 0; i < 20; i++)
             {
 
-                for (u_int8_t p = 0; p < 6; p++)
+                for (u_int8_t p = 0; p < 7; p++)
                 {
                     for (u_int8_t j = 0; j < iter; j++)
                     {
@@ -106,6 +106,9 @@ int main()
                 filter_val[3] += (pseumed[3] - filter_val[3]) * 0.05f;
                 filter_val[4] += (pseumed[4] - filter_val[4]) * 0.05f;
                 filter_val[5] += (pseumed[5] - filter_val[5]) * 0.05f;
+                filter_val[6] += (pseumed[6] - filter_val[6]) * 0.05f;
+               // uint16_t vref12 = filter_val[6] .read_u16() >> 4;
+               // float vref = (3.0f * (*VREFINT_CAL_ADDR) / vref12 );
             }
         }
         //thread_sleep_for(1000);
